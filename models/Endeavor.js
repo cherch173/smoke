a88ae28607a9e93c91fe814f89154f428666d42d
Cherch <b.cherchiglia@gmail.com>
2023-07-07 13:43:12 -0400

    comment schema added to Endeavor Model

--- models/Endeavor.js
+++ models/Endeavor.js
@@ -4,11 +4,24 @@ const endeavorSchema = new Schema(
     {
         name: { type: String, required: true },
         image: { type: Image },
+        genres: [genreSchema],
         website: { type: URL },
         description: { type: String },
         fireButton: { type: Boolean, default: false },
+        comments: [commentSchema]
     },
     { timestamps: true }
+)
+
+const genreSchema = new Schema(
+    {
+        genre: {
+            type: String,
+            enum: ['Animation', 'Art', 'App', 'Article', 'Biology', 'Blog', 'Book', 'Chemistry', 'Comedy', 'Experiment', 'Film', 'Gaming', 'Graphic Design', 'Intellectual Property', 'Invention', 'Music', 'Musical Theater', 'Play', 'Poetry', 'Programming', 'Physics', 'Science', 'Short', 'Space', 'Stand-Up Comedy', 'Theorem', 'Theory', 'Thesis', 'Video Game', 'Web Application', 'Other'],
+            required: true,
+            max: 3
+        }
+    }
 )
 
 module.exports = endeavorSchema
