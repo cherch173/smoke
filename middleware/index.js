fe839bb0f6c9c4f1bb7fc3ae571b230810b24ce3
Cherch <b.cherchiglia@gmail.com>
2023-07-07 18:03:36 -0400

    Index.js in Middleware built

--- middleware/index.js
+++ middleware/index.js
@@ -19,3 +19,40 @@ const createToken = (payload) => {
     let token = jwt.sign(payload, APP_SECRET)
     return token
 }
+
+const stripToken = (req, res, next) => {
+    try {
+        const token = req.headers['authorization'].split(' ')[1]
+        if (token) {
+            res.locals.token = token
+            return next()
+        }
+        res.status(401).send({ status:'Error', msg: 'Unauthorized'})
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({ status: 'Error', msg: 'Strip Token Error' })
+    }
+}
+
+const verifyToken = (req, res, next) => {
+    const { token } = res.locals
+    try {
+        let payload = jwt.verify(token, APP_SECRET)
+        if (payload) {
+            res.locals.payload = payload
+            return next()
+        }
+        res.status(401).send({ status: 'Error', msg: 'Unauthorized'})
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({ status: 'Error', msg: 'Verify Token Error'})
+    }
+}
+
+module.exports = {
+    hashPassword,
+    comparePassword,
+    createToken,
+    stripToken,
+    verifyToken
+}
