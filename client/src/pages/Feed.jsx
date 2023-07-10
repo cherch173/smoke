ce676ef99174dcfb5c39f173ad14bea65af5d275
Cherch <b.cherchiglia@gmail.com>
2023-07-10 17:37:50 -0400

    get Backend Data to display on main Feed

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,15 +1,47 @@
 import React from 'react'
-
+import { useEffect, useState } from 'react'
+import { GetEndeavors } from '../services/EndeavorServices'
+import { useNavigate } from 'react-router-dom'
 
 const Feed = () => {
-  return (
-    <div>
-      <p>sup</p>
-      <p>yo</p>
-      <p>im an endeavor</p>
-      <p>go banana</p>
-    </div>
-  )
+    let navigate = useNavigate()
+    const [endeavors, setEndeavors] = useState([])
+
+    useEffect(() => {
+        const handleEndeavors = async () => {
+            const data = await GetEndeavors()
+            setEndeavors(data)
+        }
+        handleEndeavors()
+    }, [])
+    return (
+        <div className="grid col=4">
+            {endeavors.map((endeavor) => (
+                <div className="card" key={GetEndeavors.id}>
+                    <h4>{endeavor.name}</h4>
+                    <p>
+                        <span className='fireText'>website: </span> 
+                        <a className="endeavorLink" href={endeavor.website}>{endeavor.website}</a>
+                    </p>
+                    <p>
+                        <span className='fireText'>description: </span> 
+                        {endeavor.description}
+                    </p>
+                    <p>
+                        <span className='fireText'>genres: </span> 
+                        {endeavor.genres}
+                    </p>
+                    {/* <FireButton /> */}
+                    <p>
+                        <span className='fireText'>comments: </span> 
+                        {endeavor.comments}
+                    </p>
+
+                </div>
+            ))}
+            
+        </div>
+    )
 }
 
 
