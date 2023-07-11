418b10500ee52ac1fa982e570d82d42221d302fe
Cherch <b.cherchiglia@gmail.com>
2023-07-11 13:03:38 -0400

    minor home page css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h4>Welcome to SMOKE</h4>
+      <h4>Welcome to <span className="fireText">SMOKE</span></h4>
       <p><i>...where there's smoke, there's <span className="fireText">fire</span>.</i></p>
     </div>
   )
