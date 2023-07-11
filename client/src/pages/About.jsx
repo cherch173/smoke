9cc33ad4763bbbdbb1b08a87e89c5d1aec7df0c4
Cherch <b.cherchiglia@gmail.com>
2023-07-11 10:46:22 -0400

    about page edited to include comments

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -13,10 +13,13 @@ const About = () => {
 
             <p>Welcome to <span className="fireText">SMOKE</span></p>
             <p className="textGeneral">
-                <span className="fireText">Smoke </span> is an interactive social network designed to proprel intellectual property easily and efficiently to peers and fellow creatives for review, general sharing purposes and healthy, encouraging critque.
+                <span className="fireText">Smoke </span> is an interactive social network designed to proprel intellectual property easily and efficiently to peers and fellow creatives for review, sharing purposes and healthy critque.
             </p>
             <p className='textGeneral'>
                 Our users can interact with each post and (if impressed) reward the Endeavor with a <span className='fireText'>flame</span> to show support.
+            </p>
+            <p className='textGeneral'>
+                Have more to share? Feel free to engage in each Endeavor's individualized <span className="fireText">comment </span> thread to share specifics.
             </p>
             <p className='textGeneral'>
                 After all, we know that where there's <span className='fireText'>smoke</span>, there's <span className='fireText'>fire</span>.
